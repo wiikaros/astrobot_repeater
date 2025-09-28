9bfa53f710c9c8691070a07db2dcafa3dd197588
Synapseeeee <151120700+Synapseeeee@users.noreply.github.com>
2025-09-28 14:59:44 +0800

    v1.0

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 from astrbot.api.event import filter, AstrMessageEvent, MessageEventResult
 from astrbot.api.star import Context, Star, register
+from astrbot.core import AstrBotConfig
 from astrbot.api import logger
+from astrbot.core.star.filter.event_message_type import EventMessageType
 
-@register("helloworld", "YourName", "一个简单的 Hello World 插件", "1.0.0")
-class MyPlugin(Star):
-    def __init__(self, context: Context):
+@register("repeater", "wiikaros", "一个简单的复读插件", "1.0.0")
+class repeater(Star):
+    def __init__(self, context: Context, config: AstrBotConfig):
         super().__init__(context)
+        self.repeat_group_whitelist = []  # 允许复读的群组列表
+        self.repeat_target_whitelist = []  # 允许复读的对象列表
+        self.repeat_target_format: dict = config.get("repeat_target_format", {})
 
     async def initialize(self):
         """可选择实现异步的插件初始化方法，当实例化该插件类之后会自动调用该方法。"""
@@ -19,6 +24,30 @@ class MyPlugin(Star):
         message_chain = event.get_messages() # 用户所发的消息的消息链 # from astrbot.api.message_components import *
         logger.info(message_chain)
         yield event.plain_result(f"Hello, {user_name}, 你发了 {message_str}!") # 发送一条纯文本消息
+
+    # 复读处理
+    @filter.message_type(EventMessageType.GROUP_MESSAGE)
+    async def repeat_handler(self, event: AstrMessageEvent):
+        # 群白名单
+        group_id = event.get_group_id()
+        if self.repeat_group_whitelist and group_id not in self.repeat_group_whitelist:
+            return
+        # 目标白名单
+        sender_id = event.get_sender_id()
+        if self.repeat_target_whitelist and sender_id not in self.repeat_target_whitelist:
+            return
+        # 复读格式
+        if not self.repeat_target_format.get("text", True) and event.message_type == EventMessageType.TEXT:
+            return
+        if not self.repeat_target_format.get("image", True) and event.message_type == EventMessageType.IMAGE:
+            return
+        # 进行复读
+        if event.message_type == EventMessageType.TEXT:
+            # await event.reply(event.message_str)
+            await event.plain_result("文字复读: " + event.message_str)
+        elif event.message_type == EventMessageType.IMAGE:
+            await event.image_result("图片复读: " + event.message_image)
+        
 
     async def terminate(self):
         """可选择实现异步的插件销毁方法，当插件被卸载/停用时会调用。"""
